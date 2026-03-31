375b56bac5b77beb491ed47b91989b4d2f7a7e2e
BATHINI  INDRA SENA REDDY <148757680+indrareddy12@users.noreply.github.com>
2026-03-31 11:42:09 +0530

    Update rag_demo.py

--- rag_demo.py
+++ rag_demo.py
@@ -3,7 +3,7 @@ import streamlit as st
 from langchain.embeddings import BedrockEmbeddings
 from langchain.llms.bedrock import Bedrock
 from langchain.document_loaders import PyPDFDirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter   
 from langchain.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
